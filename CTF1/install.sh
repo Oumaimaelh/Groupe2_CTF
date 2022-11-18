6188abe97d3546545b93232b1b3008f033016049
Oumaimaelh <81980268+Oumaimaelh@users.noreply.github.com>
2022-11-18 13:36:22 +0100

    Update install.sh

--- CTF1/install.sh
+++ CTF1/install.sh
@@ -14,10 +14,19 @@ sudo apt update
 sudo git clone --branch master ************************************************.git 
 sudo cd command_line
 npm install 
-
-
-
-
+sudo apt-get install gnupg
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+echo "deb http://repo.mongodb.org/apt/debian buster/mongodb-org/4.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+sudo systemctl daemon-reload
+sudo systemctl start mongod
+sudo systemctl enable mongod
 sudo a2enmod proxy proxy_http rewrite headers expires
 sudo touch /etc/apache2/sites-available/tomygrpCmd.com.conf
 sudo echo <VirtualHost *:80> \ServerName example.com \ServerAlias www.example.com \ProxyRequests Off \ProxyPreserveHost On \ProxyVia Full \<Proxy *> \Require all granted \</Proxy> \ProxyPass / http://127.0.0.1:3000/ \ProxyPassReverse / http://127.0.0.1:30000/ \</VirtualHost> >> /etc/apache2/sites-available/example.com.conf
@@ -28,3 +37,8 @@ sudo npm install pm2 -g
 sudo pm2 start ./app.js --name tomygrpCmd.com
 sudo pm2 save
 sudo pm2 startup
+#si on a une erreur faut juste modifier le fichier de mongodb
+
+
+
+
