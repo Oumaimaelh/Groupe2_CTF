e56e0142704c4a11fe48587011c1fa471879344e
Oumaimaelh <81980268+Oumaimaelh@users.noreply.github.com>
2022-11-28 14:29:09 +0100

    Update install.sh

--- CTF2/install.sh
+++ CTF2/install.sh
@@ -9,11 +9,11 @@ sudo apt update
 sudo apt install cron
 sudo systemctl enable cron
 #rc-update add cronie default
-echo /5 * * * * user2 /etc/cron.d/backup.sh >> /etc/crontab
+echo /5 * * * * user2 /etc/cron.d/backup.sh > /etc/crontab
 mkdir /home/user2/data
 mkdir /mnt/backup
 touch /etc/cron.d/backup.sh
-echo #!/bin/bash \cp /home/user2/data /mnt/backup/backup /etc/cron.d/backup.sh >> /etc/cron.d/backup.sh
+echo #!/bin/bash \cp /home/user2/data /mnt/backup/backup /etc/cron.d/backup.sh > /etc/cron.d/backup.sh
 chmod +x /etc/cron.d/backup.sh
 /etc/init.d/cron start
 
