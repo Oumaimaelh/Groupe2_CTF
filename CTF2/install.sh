7cf7e9722b0020f1d8580f24ceafd9445ae2eb31
Skynericc <74378395+Skynericc@users.noreply.github.com>
2022-12-01 16:09:03 +0100

    Update install.sh

--- CTF2/install.sh
+++ CTF2/install.sh
@@ -18,38 +18,38 @@ sudo systemctl enable ssh
 ##############################################################################################################
 # I) Tools of WP
 # Installation de docker 
-sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get remove docker docker-engine docker.io containerd runc 
 #Install using the repository
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg lsb-release
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 sudo docker run hello-world
 
 #from wordpress on github
-git clone https://github.com/kassambara/wordpress-docker-compose
-cd wordpress-docker-compose | sudo chown $USER /var/run/docker.sock 
-cd wordpress-docker-compose | make autoinstall
+sudo git clone https://github.com/kassambara/wordpress-docker-compose
+sudo cd wordpress-docker-compose | sudo chown $USER /var/run/docker.sock 
+sudo cd wordpress-docker-compose | make autoinstall
 
 
 ##############################################################################################################
 sudo apt update
-sudo apt install cron
+sudo apt install cron -y
 sudo systemctl enable cron
 #rc-update add cronie default
-echo /5 * * * * user2 /etc/cron.d/backup.sh > /etc/crontab
-mkdir /home/user2/data
-mkdir /mnt/backup
-touch /etc/cron.d/backup.sh
-echo #!/bin/bash \cp /home/user2/data /mnt/backup/backup /etc/cron.d/backup.sh > /etc/cron.d/backup.sh
-chmod +x /etc/cron.d/backup.sh
-/etc/init.d/cron start
+sudo echo "/5 * * * * user2 /etc/cron.d/backup.sh" > /etc/crontab
+sudo mkdir /home/user2/data
+sudo mkdir /mnt/backup
+sudo touch /etc/cron.d/backup.sh
+sudo echo -e "#!/bin/bash \ncp /home/user2/data /mnt/backup/backup /etc/cron.d/backup.sh" > /etc/cron.d/backup.sh
+sudo chmod +x /etc/cron.d/backup.sh
+sudo /etc/init.d/cron start
 
