17db1168adc92f3a79018faae19b91d46cadd6eb
thanhnguyen287 <75072526+thanhnguyen287@users.noreply.github.com>
2022-12-29 15:48:23 +0000

    update machine1.sh

--- CTF2/machine1.sh
+++ CTF2/machine1.sh
@@ -64,10 +64,10 @@ sudo cp public /wordpress_docker/wordpress
 sudo cp private /wordpress_docker/wordpress
 
 #Hint for next machine
-sudo su -c "echo 'tomy{8f3dbcbb884d161fb6eade138ed8c32e}' > /home/knight/flag4.txt" knight
+sudo su -c "echo 'tomy{good_knight}' > /home/knight/flag4.txt" knight
 sudo chmod 600 /home/knight/flag4.txt
 sudo su -c "echo 'bishop - 02719440e19a8e087f16d1124defc9ace9e29b29' > /home/knight/login.txt" knight
-sudo chmod 600 /home/knight/flag4.txt
+sudo chmod 600 /home/knight/login.txt
 #Remove scripts
 cd 
 sudo rm scriptPasswdUser1.sh
