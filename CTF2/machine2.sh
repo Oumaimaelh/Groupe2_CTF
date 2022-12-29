4c545afa1a37cd850a1c65c4d8a9c63312b90ce4
thanhnguyen287 <thanhnguyenvinh01@gmail.com>
2022-12-29 02:20:18 +0100

    machine2.sh

---
+++ CTF2/machine2.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+#Creating user
+cd
+touch scriptPasswd.sh
+sudo echo "echo 'debian:123' | chpasswd" > scriptPasswd.sh
+sudo chmod +x scriptPasswd.sh
+sudo ./scriptPasswd.sh
+
+touch scriptPasswdUser1.sh
+sudo useradd -m -s /bin/bash bishop
+sudo echo "echo 'bishop:hijohn' | chpasswd" > scriptPasswdUser1.sh
+sudo chmod +x scriptPasswdUser1.sh
+sudo ./scriptPasswdUser1.sh
+
+touch scriptPasswdUser2.sh
+sudo useradd -m -s /bin/bash rook
+sudo echo "echo 'rook:hijohn' | chpasswd" > scriptPasswdUser2.sh
+sudo chmod +x scriptPasswdUser2.sh
+sudo ./scriptPasswdUser2.sh
+
+touch scriptPasswdUser3.sh
+sudo useradd -m -s /bin/bash -u 1009 queen
+sudo echo "echo 'queen:tomygroup' | chpasswd" > scriptPasswdUser3.sh
+sudo chmod +x scriptPasswdUser3.sh
+sudo ./scriptPasswdUser3.sh
+
+sudo systemctl enable ssh
+
+#########################CRONTAB VULN######################################
+
+sudo apt update
+sudo apt install cron -y
+sudo systemctl enable cron
+sudo chmod o+w
+
+sudo echo "*/5 * * * * rook /home/rook/backup.sh" | sudo tee -a /etc/crontab
+sudo su -c 'mkdir /home/rook/data' rook
+sudo su -c 'mkdir /mnt/backup' rook
+sudo su -c 'touch /home/rook/backup.sh' rook
+sudo su -c  "echo 'tomy{Cr0n_i5_g0n3}' > /home/rook/flag5.txt" rook
+sudo chmod 600 /home/rook/flag5.txt
+sudo su -c  "echo -e '#!/bin/bash \ncp -r /home/rook/data /mnt/backup' > /home/rook/backup.sh" rook
+sudo chmod 777 /home/rook/backup.sh
+sudo /etc/init.d/cron start
+
+########################PATH VARIABLE VULN#######################################
+cd /home/queen
+sudo su -c 'wget https://raw.githubusercontent.com/thanhnguyen287/SQL-Vulnerable/main/queenscript.c' queen
+sudo su -c 'gcc -o reveal queenscript.c' queen
+sudo su -c 'chmod u+s reveal' queen
+sudo su -c "echo -e 'Is this the real life?\nIs this just fantasy?' > /home/queen/msg" queen
+sudo chmod 666 /home/queen/msg
+sudo su -c  "echo 'tomy{Path_Variable_6}' > /home/queen/flag6.txt" queen
+sudo chmod 600 /home/queen/flag6.txt
+
+sudo rm queenscript.c
+
+########################BUFFER OVERFLOW#######################################
+cd /
+sudo wget https://github.com/thanhnguyen287/SQL-Vulnerable/blob/main/mount_pwn?raw=true
+sudo mv 'mount_pwn?raw=true' mount_pwn
+sudo echo "0" | sudo tee /proc/sys/kernel/randomize_va_space
+sudo chmod u+s mount_pwn
+sudo echo "tomy{pawn_r00t_fl4g}" | sudo tee /root/flag_root.txt
+
+
